a13dac0d30ff9fff9f6b69eec98698df9a724477
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-06-10 14:06:23 +0530

    rf

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -14,20 +14,26 @@ require("dotenv").config();
 //     })
 // }
 router.post('/register', async (req, res) => {
-   User.create(req.body, (error, user) => {
-           if (error) {
-               return res.send({error})
-           }
-           res.send({
-                    
-                     _id: user.id,
-                     name: user.name,
-                     email: user.email,
-                     isAdmin: user.isAdmin,
-                     token: getToken(user)
-                 })
-       })
-      })  
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser)
+    })
+  } else {
+    res.status(401).send({ msg: 'Invalid User Data.' });
+  }
+
+})
+
       router.post('/signin', async (req, res) => {
 
         const signinUser = await User.findOne({
