eb63e86866e0520b417b7d1900a6d98c225224dd
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-06-10 12:53:40 +0530

    ss

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -49,36 +49,39 @@ router.post('/register', async (req, res) => {
       
       });
     
-      router.put('/:id',  async (req, res) => {
-        const token = req.headers.authorization;
+      // router.put('/:id',  async (req, res) => {
+      //   const token = req.headers.authorization;
+      //   const userId = req.params.id;
+      //   jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      //     if (err) {
+      //       return res.status(401).send({ msg: 'Invalid Token' });
+      //     }
+      //     req.user = decode;
+      //     res.send(req.user)
+      //   });
+      // });
+      router.put('/:id', isAuth, async (req, res) => {
         const userId = req.params.id;
-        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
-          if (err) {
-            return res.status(401).send({ msg: 'Invalid Token' });
-          }
-          req.user = decode;
-          res.send(req.user)
-        });
+        const user = await User.findById(userId);
+        if (user) {
+          user.name = req.body.name || user.name;
+          user.email = req.body.email || user.email;
+          user.password = req.body.password || user.password;
+          const updatedUser = await user.save();
+          res.send({
+            _id: updatedUser.id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: getToken(updatedUser)
+          });
+        } 
+        else {
+          res.status(404).send({ msg: 'User Not Found' });
+        }
+      
       });
+      
         
-// router.post('/register', async (req, res) => {
-//     const user = new User({
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: req.body.password
-//     });
-//     const newUser = await user.save();
-//     if (newUser) {
-//       res.send({
-//         _id: newUser.id,
-//         name: newUser.name,
-//         email: newUser.email,
-//         isAdmin: newUser.isAdmin,
-//         //token: getToken(newUser)
-//       })
-//     } else {
-//       res.status(401).send({ msg: 'Invalid User Data.' });
-//     }
-  
-//   })
+
   module.exports= router;
