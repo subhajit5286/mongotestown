8f8012164310948443d95bde2cba41bdd0b9bbff
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-06-10 10:22:03 +0530

    router modified

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -11,23 +11,31 @@ const router = express.Router();
 //     })
 // }
 router.post('/register', async (req, res) => {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    const newUser = await user.save();
-    if (newUser) {
-      res.send({
-        _id: newUser.id,
-        name: newUser.name,
-        email: newUser.email,
-        isAdmin: newUser.isAdmin,
-        //token: getToken(newUser)
-      })
-    } else {
-      res.status(401).send({ msg: 'Invalid User Data.' });
-    }
+      User.create(req.body, (error, user) => {
+           if (error) {
+               return res.send({error})
+           }
+           res.send('user creation success')
+       })
+      })    
+// router.post('/register', async (req, res) => {
+//     const user = new User({
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: req.body.password
+//     });
+//     const newUser = await user.save();
+//     if (newUser) {
+//       res.send({
+//         _id: newUser.id,
+//         name: newUser.name,
+//         email: newUser.email,
+//         isAdmin: newUser.isAdmin,
+//         //token: getToken(newUser)
+//       })
+//     } else {
+//       res.status(401).send({ msg: 'Invalid User Data.' });
+//     }
   
-  })
+//   })
   module.exports= router;
