dda4f84d5d52a2116a8d524414959556f062f6b3
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-06-10 14:16:46 +0530

    admin

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -87,7 +87,21 @@ router.post('/register', async (req, res) => {
         }
       
       });
-      
+      router.get("/createadmin", async (req, res) => {
+        try {
+          const user = new User({
+            name: 'Subhajit',
+            email: 'subhajit@gmail.com',
+            password: '1234',
+            isAdmin: true
+          });
+          const newUser = await user.save();
+          res.send(newUser);
+        } catch (error) {
+          res.send({ msg: error.message });
+        }
+      });
+    
       
         
 
