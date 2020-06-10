48a254aa081193c38b8125194171547b996b04f6
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-06-10 13:27:18 +0530

    a

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -62,24 +62,14 @@ router.post('/register', async (req, res) => {
       // });
       router.put('/:id', isAuth, async (req, res) => {
         const userId = req.params.id;
-        const user = await User.findById(userId);
-        if (user) {
-          user.name = req.body.name || user.name;
-          user.email = req.body.email || user.email;
-          user.password = req.body.password || user.password;
-          const updatedUser = await user.save();
-          res.send({
-            _id: updatedUser.id,
-            name: updatedUser.name,
-            email: updatedUser.email,
-            isAdmin: updatedUser.isAdmin,
-            token: getToken(updatedUser)
-          });
-        } 
-        else {
-          res.status(404).send({ msg: 'User Not Found' });
-        }
-      
+        User.findByIdAndUpdate(req.params.id,req.body).then (function() {
+          User.findById(req.params.id).then (function(user) {
+          res.send(user)
+          })
+          
+   }).catch(function(err){
+      res.send('err')
+  })    
       });
       
         
