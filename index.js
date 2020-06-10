0ba557452db7f574edec25c6b16ff4e3ecd279a4
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-06-10 15:20:46 +0530

    a

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ else{
     console.log({error})
 }
 })
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+    next();
+  });
+  
 app.get('/',(req,res)=>{
     res.send('Hello');
 })
