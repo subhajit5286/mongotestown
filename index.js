7d783dec565ff5beb585d9123e236a5304c88830
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-06-11 13:33:59 +0530

    s

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ app.get("/api/config/paypal", (req, res) => {
 
 var port = process.env.PORT || 5000;
   
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 
 app.listen(port,()=>{
     console.log("Server started at http://localhost:5000")
